55eccb4643876a63cd1d5b0c18e0cb18173f35b2
dev-crisis66 <thefrostandfire@gmail.com>
2024-06-20 10:10:26 +0200

    first commit initial base

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
-export default function Home() {
+import {sql} from "@vercel/postgres";
+
+export default async function Home() {
+    const {rows} = await sql`SELECT * FROM CARS;`;
     return (
+        <>
+            {rows.map((row, index) => {
+                return (
+                    <ul>
+
+                        <li>{row.brand}</li>
+                        <li>{row.model}</li>
+                        <li>{row.year}</li>
+                    </ul>
+                )
+            })}
             <h1>Home Page</h1>
+        </>
     );
 }
